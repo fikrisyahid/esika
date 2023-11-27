66383e3c1d818898cef27f51dfbce8bb1a18b719
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2023-11-27 23:39:26 +0700

    fix: generate code util

--- src/utils/generate-code.js
+++ src/utils/generate-code.js
@@ -1,11 +1,10 @@
 /**
  * Generates a random code of specified length.
  *
- * @param {Object} options - The options for generating the code.
- * @param {number} [options.length=4] - The length of the code to be generated.
+ * @param {number} [length=4] - The length of the code to be generated.
  * @returns {string} The generated code.
  */
-export default function generateCode({ length = 4 }) {
+export default function generateCode(length = 4) {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   let code = "";
   Array.from({ length }).forEach(() => {
