4e910d295ed39f6523f98e72cf117441479dcc33
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2023-11-28 14:45:51 +0700

    feat: work in progress component and implementation

--- src/pages/admin/users.jsx
+++ src/pages/admin/users.jsx
@@ -1,13 +1,10 @@
-import MainCard from "@/components/MainCard";
 import PageWrapper from "@/components/PageWrapper";
-import { Text } from "@mantine/core";
+import WorkInProgress from "@/components/WorkInProgress";
 
 export default function users() {
   return (
     <PageWrapper pageTitle="Users">
-      <MainCard>
-        <Text>Ini halaman users buat admin</Text>
-      </MainCard>
+      <WorkInProgress />
     </PageWrapper>
   );
 }
--- src/pages/teacher/dashboard.jsx
+++ src/pages/teacher/dashboard.jsx
@@ -1,13 +1,10 @@
-import MainCard from "@/components/MainCard";
 import PageWrapper from "@/components/PageWrapper";
-import { Text } from "@mantine/core";
+import WorkInProgress from "@/components/WorkInProgress";
 
 export default function Dashboard() {
   return (
     <PageWrapper pageTitle="Dashboard">
-      <MainCard>
-        <Text>Contoh teks</Text>
-      </MainCard>
+      <WorkInProgress />
     </PageWrapper>
   );
 }
